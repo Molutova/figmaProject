29e713f9266f3c42ec2b30db31d8b09b361b835c
Nailya <nmolutova@gmail.com>
2023-04-28 16:46:37 +0600

    new landing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector(".burger-menu-box");
+  const btn = document.querySelector(".my-button");
   btn.addEventListener("click", function () {
     const menuList = document.querySelector("ul");
     console.log(menuList);
     menuList.classList.add("toogle");
+    document.querySelector("color-text");
+    const colortxt = document.querySelector(".color-text");
+    // colortxt.classList.remove("color-text");
   });
 });
